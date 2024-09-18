4bc4c28fd0f118613a52d8599230b2e88fe107e3
egouhj5 <kn09032010@gmail.com>
2024-09-18 22:20:25 +0300

    Homework is finaly donea

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//1
+
+// let arr = ["Dima-1", "Dima-2", "Dima-3", "Dima-4"];
+// let result = "";
+
+// for (let i = 0; i < arr.length; i++) {
+//   result += arr[i];
+//   if (i < arr.length - 1) {
+//     result += ",";
+//   }
+// }
+
+// console.log(result);
+// console.log(arr.join(","));
+
+//2
+
+// const arr1 = ["Card-1", "Card-2", "Card-3", "Card-4", "Card-5"];
+
+// console.log(arr1.slice(4));
+
+//3-4-5
+
+// const arr2 = ["Card-1", "Card-2", "Card-3", "Card-4", "Card-5"];
+
+// console.log(arr2);
+// const index = arr2.indexOf("Card-3");
+// arr2.splice(index, 1);
+// console.log(arr2);
+// const cardToInsert = "Card-6";
+// arr2.push(cardToInsert);
+// console.log(arr2);
+// const cardToUpdate = arr2.indexOf("Card-4");
+// arr2[cardToUpdate] = "Vip-Card";
+// console.log(arr2);
